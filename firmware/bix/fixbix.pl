66e19a12b06ab4740dbdfd17c600b31814b5851c
Ether Hack <etherhack@mailinator.com>
2010-04-21 00:51:29 +0200

    maybe fix printing of checksums

--- firmware/bix/fixbix.pl
+++ firmware/bix/fixbix.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 
 my $header;    # working copy of the header
-my $ixor;      # working copy of the image checksum
-my $hxor;      # working copy of the header checksum
-my $size;      # length of the image
+my $ixor = 0;  # working copy of the image checksum
+my $hxor = 0;  # working copy of the header checksum
+my $size = 0;  # length of the image
 
 open( BIX, "+<", $ARGV[0] );
 if ( read( BIX, $header, 28 ) != 28 ) { die "Error while reading bix"; }
@@ -17,19 +18,21 @@ while ( my $length = read( BIX, my $data, 4 ) ) {
     $ixor = $ixor ^ $data;
 }
 
-$header
-    = substr( $header, 0, 4 )
-    . pack( "N", $ixor )
-    . substr( $header, 8, 4 )
-    . pack( "N", $size )
-    . substr( $header, 16, 12 );
+$header =
+    substr( $header, 0, 4 )
+  . pack( "N", $ixor )
+  . substr( $header, 8, 4 )
+  . pack( "N", $size )
+  . substr( $header, 16, 12 );
 
-for ( my $counter = 0; $counter < length $header; $counter = $counter + 4 ) {
+for ( my $counter = 0 ; $counter < length $header ; $counter = $counter + 4 ) {
     my $data = substr( $header, $counter, 4 );
+    $data = unpack( "N", $data );
     $hxor = $hxor ^ $data;
 }
 
-$header = $header . $hxor;
+my $packed_hxor = pack( "N", $hxor );
+$header = $header . $packed_hxor;
 
 seek( BIX, 0, 0 );
 print BIX $header;
